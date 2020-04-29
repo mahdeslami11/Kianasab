6fe317dcb1fada8510ab56faf50eb5c89e7fa425
William Marstrand <william.marstrand@gmail.com>
2020-04-29 09:20:56 +0200

    corrected file ending check

--- preprocess/preprocess_vctk.py
+++ preprocess/preprocess_vctk.py
@@ -6,9 +6,10 @@ def save_path_if_valid(log:Logger, folder_path:str):
     num_of_wav = 0
 
     with open(join('vctk', 'speaker_paths_vctk.txt', 'w+')) as sp:
-        for wav in listdir(folder_path) if wav[-4] == '.wav':
-            sp.write(f'{wav}\n')
-            num_of_wav += 1
+        for wav in listdir(folder_path):
+            if wav[-4] == '.wav':
+                sp.write(f'{wav}\n')
+                num_of_wav += 1
 
     log.write(f'Found {num_of_wav} wav files for speaker')
 
