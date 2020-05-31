44be6158ee0f32bd16cf3da7e5302b5f6a9b76e1
William Marstrand <william.marstrand@gmail.com>
2020-05-31 22:36:08 +0200

    added reading of meta data into dictionary

--- preprocess/spraakbanken/meta.py
+++ preprocess/spraakbanken/meta.py
@@ -1,5 +1,4 @@
-from os.path import join
-from os import listdir
+import glob
 
 #Find spl file from speaker id
 #Split spl file on newlines
@@ -19,16 +18,57 @@ from os import listdir
         #Remove if split array entry is empty
 #Print meta data to json with indent=4
 
-def read_spl_file(fpath:str):
-    info = fpath.split('_')
-    station = info[0]
-    station_id = station[7:].replace('0', '')
-    substation = info[1]
-    speaker = info[2]
+def read_record_states(c, info_dict):
+     delimiter = info_dict['delimiter']
+     content = c.split(delimiter)
+     utterance =  content[2]
+     if utterance[1:3] is not '...':
+        info_dict[content[5]] = utterance
 
-    for f in listdir(join('/work1/s183921/speaker_data/Spraakbanken-Raw',station,substation,'adb_0565','data','scr0565',f'0{station_id}',speaker)):
-        with open(f, 'r') as spl:
-            spl_content = spl.readlines()
-            spl_content = spl_content.split('\n\n')
-            print(spl_content[0])
 
+def read_info_states(c, info_dict):
+    right_side = c.split('=')[1]
+    delimiter = info_dict['delimiter']
+    if str.startswith(right_side, 'age'):
+        info_dict['age'] = right_side.split(delimiter)[1]
+    elif str.startswith(right_side, 'sex'):
+        info_dict['sex'] = right_side.split(delimiter)[1]
+    elif str.startswith(right_side, 'region of dialect'):
+        info_dict['dialect'] = right_side.split(delimiter)[1]
+
+def read_system_info(c, info_dict):
+    if str.startswith(c, 'delimiter'):
+        info_dict['delimiter'] = c.split('=')[1]
+    elif str.startswith(c, 'frequency'):
+        info_dict['frequency'] = c.split('=')[1]
+
+
+def read_spl_file(speaker_id:str):
+    '''
+    Reads the content of a speaker .spl info file for the speaker with the given id
+
+    :param speaker_id:  Id of the speaker to find and read the .spl info file for
+
+    :returns:           A dictionary with the read speaker info
+    '''
+    info_dict = {}
+    info_dict['speaker'] = speaker_id
+
+    #Change the path to fit your own file structure if needed
+    spl_file = glob.glob(f'/work1/s183921/speaker_data/Spraakbanken-Raw/*/*/*/data/*/*/*/{speaker_id}.spl')[0]
+
+    with open(spl_file, 'r') as spl:
+        spl = spl.read().lower()
+        spl = spl.split('\n\n')
+        
+        for f in spl:
+            content = f.split('\n')
+            for c in content:
+                if content[0] == '[system]':
+                    read_system_info(c, info_dict)
+                elif content[0] == '[info states]':
+                    read_info_states(c, info_dict)
+                elif content[0] == '[record states]':
+                    read_record_states(c, info_dict)
+
+        return info_dict
