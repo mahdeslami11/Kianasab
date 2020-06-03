7d422c8a7017f1fa62d370096cf25100b0fff214
William Marstrand <william.marstrand@gmail.com>
2020-06-03 13:55:32 +0200

    changed inference back to original

--- vc/vae/inference.py
+++ vc/vae/inference.py
@@ -1,11 +1,14 @@
+import sys
+sys.path.append('/work1/s183921/fagprojekt2020')
 import torch
 import numpy as np
-import sys
-import os 
+import os
 import torch.nn as nn
 import torch.nn.functional as F
 import yaml
 import pickle
+from model import AE
+from utils import *
 from functools import reduce
 import json
 from collections import defaultdict
@@ -15,40 +18,18 @@ from torch.utils.data import DataLoader
 from argparse import ArgumentParser, Namespace
 from scipy.io.wavfile import write
 import random
-import librosa 
-from vclab.vc_models.adaptiveVC_model_files.preprocess.tacotron.utils import melspectrogram2wav
-from vclab.vc_models.adaptiveVC_model_files.preprocess.tacotron.utils import get_spectrograms
-from vclab.vc_models.adaptiveVC_model_files.model import AE
-from vclab.vc_models.adaptiveVC_model_files.utils import *
-from vclab.vc_models.test_model import TestModel
+from preprocess.tacotron.utils import melspectrogram2wav
+from preprocess.tacotron.utils import get_spectrograms
+import librosa
 
-class Inference(TestModel):
-    """
-    Class representing a rewrite of the inference.py from 
-    the voice conversion model in the Adaptive Voice Conversion Paper:
-    https://github.com/jjery2243542/adaptive_voice_conversion
-    """
-    def __init__(self,sample_rate:int=24000):
-        """
-        Initialization method
-        
-        :param config_file:     Path to yaml configuration file which will be used to load the settings for 
-                                the voice conversion. 
-        :param attr_file:       Path to the pickle file generated from the model training used to set attributes
-                                for the voice conversion
-        :param model_path:      Path to the trained adaptive voice conversion model to use for the voice conversion
-        :param sample_rate:     The sample rate to use when saving the converted voice to a wav file
-        """
-        self.sample_rate = sample_rate
-        self.model_path = os.path.join(os.path.join(os.path.dirname(__file__), 'adaptiveVC_model_files'), 'models.ckpt')
-        config_file = os.path.join(os.path.join(os.path.dirname(__file__), 'adaptiveVC_model_files'), 'models.config.yaml')
-        attr_file = os.path.join(os.path.join(os.path.dirname(__file__), 'adaptiveVC_model_files'), 'attr.pkl')
-
-        with open(config_file) as f:
-            config = yaml.load(f)
+class Inferencer(object):
+    def __init__(self, config, args):
         # config store the value of hyperparameters, turn to attr by AttrDict
         self.config = config
         print(config)
+        # args store other information
+        self.args = args
+        print(self.args)
 
         # init the model with config
         self.build_model()
@@ -56,22 +37,24 @@ class Inference(TestModel):
         # load model
         self.load_model()
 
-        with open(attr_file, 'rb') as f:
+        with open(self.args.attr, 'rb') as f:
             self.attr = pickle.load(f)
 
     def load_model(self):
-        print(f'Load model from {self.model_path}')
-        self.model.load_state_dict(torch.load(self.model_path))
+        print(f'Load model from {self.args.model}')
+        self.model.load_state_dict(torch.load(f'{self.args.model}'))
+        return
 
-    def build_model(self): 
+    def build_model(self):
         # create model, discriminator, optimizers
         self.model = cc(AE(self.config))
         print(self.model)
         self.model.eval()
+        return
 
     def utt_make_frames(self, x):
         frame_size = self.config['data_loader']['frame_size']
-        remains = x.size(0) % frame_size 
+        remains = x.size(0) % frame_size
         if remains != 0:
             x = F.pad(x, (0, remains))
         out = x.view(1, x.size(0) // frame_size, frame_size * x.size(1)).transpose(1, 2)
@@ -98,13 +81,30 @@ class Inference(TestModel):
         return ret
 
     def write_wav_to_file(self, wav_data, output_path):
-        write(output_path, rate=self.sample_rate, data=wav_data)
+        write(output_path, rate=self.args.sample_rate, data=wav_data)
+        return
 
-    def convert(self, source:str, target:str, output_path:str):
-        src_mel, _ = get_spectrograms(source)
-        tar_mel, _ = get_spectrograms(target)
+    def inference_from_path(self):
+        src_mel, _ = get_spectrograms(self.args.source)
+        tar_mel, _ = get_spectrograms(self.args.target)
         src_mel = torch.from_numpy(self.normalize(src_mel)).cuda()
         tar_mel = torch.from_numpy(self.normalize(tar_mel)).cuda()
         conv_wav, conv_mel = self.inference_one_utterance(src_mel, tar_mel)
-        print(f'Writing wav file to: {output_path}')
-        self.write_wav_to_file(conv_wav, output_path)
+        self.write_wav_to_file(conv_wav, self.args.output)
+        return
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-attr', '-a', help='attr file path')
+    parser.add_argument('-config', '-c', help='config file path')
+    parser.add_argument('-model', '-m', help='model path')
+    parser.add_argument('-source', '-s', help='source wav path')
+    parser.add_argument('-target', '-t', help='target wav path')
+    parser.add_argument('-output', '-o', help='output wav path')
+    parser.add_argument('-sample_rate', '-sr', help='sample rate', default=24000, type=int)
+    args = parser.parse_args()
+    # load config file
+    with open(args.config) as f:
+        config = yaml.load(f)
+    inferencer = Inferencer(config=config, args=args)
+    inferencer.inference_from_path()
