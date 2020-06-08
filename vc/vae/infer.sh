98e952e4bd1dfc07c9bfd43b430e8e4b42b9d287
William <s183921@student.dtu.dk>
2020-06-08 14:53:23 +0200

    updating inference script

--- vc/vae/infer.sh
+++ vc/vae/infer.sh
@@ -5,10 +5,10 @@ target="/work1/s183921/speaker_data/Spraakbanken-Corpus-Test/r6110050/r6110050_u
 for source in $(ls $source_folder)
     do
         echo "Converting $source to $target..."
-        python3 inference.py    -a /work1/s183921/preprocessed_data/vae/spraakbanken/preprocessed_danish_10_test/attr.pkl
-                                -c /work1/s183921/trained_models/vae/model_danish_10_test/model.config.yaml
-                                -m /work1/s183921/trained_models/vae/model_danish_10_test/model.ckpt
-                                -t /work1/s183921/speaker_data/target_speakers/william_test/$target
+        python3 inference.py    -a /work1/s183921/preprocessed_data/vae/spraakbanken/preprocessed_danish_10_test/attr.pkl \
+                                -c /work1/s183921/trained_models/vae/model_danish_10_test/model.config.yaml \
+                                -m /work1/s183921/trained_models/vae/model_danish_10_test/model.ckpt\
+                                -t /work1/s183921/speaker_data/target_speakers/william_test/$target\
                                 -o /work1/s183921/converted_speakers/vae/$source_to_$target.wav
         echo "Completed conversion.."
     done
