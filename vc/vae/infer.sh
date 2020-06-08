d0b009523f893b7fabe7d811730fa419ec963cf2
William <s183921@student.dtu.dk>
2020-06-08 15:18:49 +0200

    added missing new line in .sh infer script

--- vc/vae/infer.sh
+++ vc/vae/infer.sh
@@ -8,7 +8,7 @@ for source in $(ls $source_folder/*.wav)
         python3 inference.py    -a /work1/s183921/preprocessed_data/vae/spraakbanken/preprocessed_danish_10_test/attr.pkl \
                                 -c /work1/s183921/trained_models/vae/model_danish_10_test/model.config.yaml \
                                 -m /work1/s183921/trained_models/vae/model_danish_10_test/model.ckpt\
-                                -s $source
+                                -s $source\
                                 -t /work1/s183921/speaker_data/target_speakers/william_test/$target\
                                 -o /work1/s183921/converted_speakers/vae/$source_to_$target.wav
         echo "Completed conversion.."
