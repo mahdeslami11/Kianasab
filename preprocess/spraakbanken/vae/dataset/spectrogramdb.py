bfc9fcc557f9ed7ee110a2328a9df7b2a4e2601a
William Marstrand <william.marstrand@gmail.com>
2020-06-13 12:36:47 +0200

    trying to fix calls to SpectrogramDB super class

--- preprocess/spraakbanken/vae/dataset/spectrogramdb.py
+++ preprocess/spraakbanken/vae/dataset/spectrogramdb.py
@@ -4,20 +4,20 @@ from tinydb import TinyDB, Query
 class SpectrogramDB(TinyDB):
     def __init__(self, db_path:str):
         self.query = Query()
-        super(SpectrogramDB, self).__init__(db_path)
+        super().__init__(db_path)
 
     def insert_spectrogram(self, name, spectrogram:ndarray):
-        super().insert({'key': name, 'val': spectrogram.tolist()})
+        self.insert({'key': name, 'val': spectrogram.tolist()})
 
     def get_spectrogram(self, name):
-        return asarray(super().search(query.key == name)[0]['val'])
+        return asarray(self.search(query.key == name)[0]['val'])
 
     def update_spectrogram(self, name, spectrogram):
-            super().update({'key': name, 'val': spectrogram.tolist()}, 
+            self.update({'key': name, 'val': spectrogram.tolist()}, 
                             query.key == name)
 
     def get_keys(self):
-        return [record['key'] for record in super().all()]
+        return [record['key'] for record in self.all()]
 
     def get_vals(self):
-        return [asarray(record['val']) for record in super().all()]
+        return [asarray(record['val']) for record in self.all()]
