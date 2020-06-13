7731c9dfee22c4ec68c1a785eed74a15fac1d64c
William Marstrand <william.marstrand@gmail.com>
2020-06-13 13:09:24 +0200

    Changed preprocess.config stage to 0

--- preprocess/spraakbanken/vae/dataset/spectrogramdb.py
+++ preprocess/spraakbanken/vae/dataset/spectrogramdb.py
@@ -2,28 +2,24 @@ import os
 from numpy import ndarray, asarray
 from tinydb import TinyDB, Query
 
-class SpectrogramDB():
+class SpectrogramDB(TinyDB):
     def __init__(self, db_path:str, overwrite=False):
         if overwrite and os.path.exists(db_path):
             os.remove(db_path)
         self.query = Query()
-        self.db = TinyDB(db_path)
 
     def insert_spectrogram(self, name, spectrogram:ndarray):
-        self.db.insert({'key': name, 'val': spectrogram.tolist()})
+        self.insert({'key': name, 'val': spectrogram.tolist()})
 
     def get_spectrogram(self, name):
-        return asarray(self.db.search(query.key == name)[0]['val'])
+        return asarray(self.search(query.key == name)[0]['val'])
 
     def update_spectrogram(self, name, spectrogram):
-            self.db.update({'key': name, 'val': spectrogram.tolist()}, 
+            self.update({'key': name, 'val': spectrogram.tolist()}, 
                             query.key == name)
 
     def get_keys(self):
-        return [record['key'] for record in self.db.all()]
+        return [record['key'] for record in self.all()]
 
     def get_vals(self):
-        return [asarray(record['val']) for record in self.db.all()]
-
-    def all(self):
-        return self.db.all()
+        return [asarray(record['val']) for record in self.all()]
