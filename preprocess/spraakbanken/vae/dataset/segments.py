991c55a78a2b6e145dfcd2d1360d4c117644706a
William Marstrand <william.marstrand@gmail.com>
2020-06-15 11:58:29 +0200

    corrected segments

--- preprocess/spraakbanken/vae/dataset/segments.py
+++ preprocess/spraakbanken/vae/dataset/segments.py
@@ -1,25 +1,24 @@
 import json 
+import pickle 
 import sys
 import os
 import random
-from spectrogramdb import SpectrogramDB
 
 if __name__ == '__main__':
-    db_path = sys.argv[1]
+    pickle_path = sys.argv[1]
     sample_path = sys.argv[2]
     n_samples = int(sys.argv[3])
     segment_size = int(sys.argv[4])
 
-    db = SpectrogramDB(db_path)
-    print(f'Spectrogram Database has {len(db.all())} records')
+    with open(pickle_path, 'rb') as f:
+        data = pickle.load(f)
 
     # (utt_id, timestep, neg_utt_id, neg_timestep)
     samples = []
 
     # filter length > segment_size
-    utt_list = db.get_keys()
-    print(f'Loaded {len(utt_list)} keys')
-    utt_list = sorted(list(filter(lambda u : len(db.get_spectrogram(u)) > segment_size, utt_list)))
+    utt_list = [key for key in data]
+    utt_list = sorted(list(filter(lambda u : len(data[u]) > segment_size, utt_list)))
     print(f'{len(utt_list)} utterances')
     sample_utt_index_list = random.choices(range(len(utt_list)), k=n_samples)
 
@@ -27,10 +26,8 @@ if __name__ == '__main__':
         if i % 500 == 0:
             print(f'sample {i} samples')
         utt_id = utt_list[utt_ind]
-        spectrogram = db.get_spectrogram(utt_id)
-        t = random.randint(0, len(spectrogram) - segment_size)
+        t = random.randint(0, len(data[utt_id]) - segment_size)
         samples.append((utt_id, t))
 
-    with open(sample_path, 'w+') as f:
+    with open(sample_path, 'w') as f:
         json.dump(samples, f)
-
